13e1838c32d6f2e145607124437d008359514ec7
Braulio Paredes <84678447+beparede@users.noreply.github.com>
2021-08-06 10:34:26 -0500

    Correcciones ortográficas y de formato

--- app/src/main/java/com/example/amst5/Infantil.java
+++ app/src/main/java/com/example/amst5/Infantil.java
@@ -17,11 +17,11 @@ public class Infantil extends AppCompatActivity {
                     "  SIPNOSIS \n Una niña recibe de su madre el encargo de llevar una cesta " +
                             "a su abuela enferma que vive en el bosque, advirtiéndole que no " +
                             "hable con desconocidos. Pero por el camino se encuentra un lobo " +
-                            "y se para a hablar con él, dándole detalles de lo que va a hacer. " +
+                            "y se para a hablar con él, dándole detalles de lo que va a hacer." +
                             "El lobo aprovecha para engañar a caperucita y llegar antes a casa " +
                             "de la abuelita, a quien se come, y luego ocupa su lugar para engañar " +
-                            "a caperucita y comérsela también. Afortunadamente, un leñador" +
-                            " que andaba por allí descubre al lobo durmiendo tras su comida," +
+                            "a caperucita y comérsela también. Afortunadamente, un leñador " +
+                            " que andaba por allí descubre al lobo durmiendo tras su comida, " +
                             " y rescata a caperucita y su abuelita de la tripa del lobo, " +
                             "sustituyéndolas por piedras que hacen que el lobo se ahogue al ir " +
                             "a beber al río."},

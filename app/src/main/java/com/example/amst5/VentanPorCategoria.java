aa5b0a164f6108cd31348374f28d6853b0e400e9
PST-proyecto <aeimaice@espol.edu.ec>
2021-08-06 00:52:09 -0500

    Descripción libro frankentein

--- app/src/main/java/com/example/amst5/VentanPorCategoria.java
+++ app/src/main/java/com/example/amst5/VentanPorCategoria.java
@@ -14,7 +14,12 @@ public class VentanPorCategoria extends AppCompatActivity {
     ListView listaTerror;
     String[][] datoster = {
             {"Titulo: Frankenstein", "Autor: Marry Shelly", "Editorial: Lackington",
-                    "LIBRO 2 BLABLABALBALABLABLABA"},
+                    "SINOPSIS:\n Frankenstein es una historia macabra en la que Víctor, un joven ávido de conocimientos científicos,"+
+            " se obsesiona por lograr el mayor reto posible en el mundo científico: dar vida a un cuerpo muerto."+
+            "Su éxito será su condena, la creación de un monstruo estremecedor que,"+ "n respuesta a su rechazo por todos se entrega por completo a"
+            +"saciar una sed de venganza hacia su creador, culpable de su desgracia,"+ "y hacia todo lo que éste ama, tornando en muerte todo"+
+            "alrededor de Víctor. El monstruo,"+"enfermo de soledad, solicita una compañera a su creador a cambio de desaparecer para siempre,"+
+            "pero Víctor se niega a ello, provocando así que la única salida hacia la"+"paz y el descanso sea el fin de uno de los dos."},
     };
 
     int[] datosImgTerror = {R.drawable.frankenstein };

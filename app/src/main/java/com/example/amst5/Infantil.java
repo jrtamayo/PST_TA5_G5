740d2cd89120a02abf5f0b0761a0d5140c4a7f6d
mnnunez <84679070+mnnunez@users.noreply.github.com>
2021-08-06 01:15:47 -0500

    Se agrega imagen: Libro Patito Feo

--- app/src/main/java/com/example/amst5/Infantil.java
+++ app/src/main/java/com/example/amst5/Infantil.java
@@ -47,7 +47,7 @@ public class Infantil extends AppCompatActivity {
                             "las más suculentas son las que están recubiertas de niño tierno."},
     };
 
-    int[] datosImgInfantil = {R.drawable.roja };
+    int[] datosImgInfantil = {R.drawable.roja, R.drawable.patitofeo};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

f446d37d72d4f4027c2a8b21e42d00b1ae838162
Braulio Paredes <84678447+beparede@users.noreply.github.com>
2021-08-06 10:31:03 -0500

    Corrección de ortografía en las sinopsis y formato

--- app/src/main/java/com/example/amst5/MainActivity3.java
+++ app/src/main/java/com/example/amst5/MainActivity3.java
@@ -21,33 +21,33 @@ public class MainActivity3 extends AppCompatActivity implements SearchView.OnQue
     String[][] datos = {
 
             {"Titulo: Harry Potter", "Autor: Joanne Rowling", "Editorial: Salamandra",
-                    " SIPNOSIS \n Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y el insoportable primo Dudley. " + 
-                                "Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre. " +
+                    " SIPNOSIS \n Harry Potter se ha quedado huérfano y vive en casa de sus abominables tíos y el insoportable primo Dudley." + 
+                                "Harry se siente muy triste y solo, hasta que un buen día recibe una carta que cambiará su vida para siempre." +
                                 "En ella le comunican que ha sido aceptado como alumno en el Colegio Hogwarts de Magia. A partir de ese momento, " +
                                 "la suerte de Harry da un vuelco espectacular. En esa escuela tan especial aprenderá encantamientos, trucos fabulosos " +
-                                "y tácticas de defensa contra las malas artes. Se convertirá en el campeón escolar de Quidditch, especie de fútbol aéreo " +
-                                "que se juega montado sobre escobas, y hará un puñado de buenos amigos... aunque también algunos temibles enemigos. " +
+                                "y tácticas de defensa contra las malas artes. Se convertirá en el campeón escolar de Quidditch, especie de fútbol aéreo." +
+                                "que se juega montado sobre escobas, y hará un puñado de buenos amigos... aunque también algunos temibles enemigos." +
                                 "Pero, sobre todo, conocerá los secretos que le permitirán cumplir su destino. Pues, aunque no lo parezca a primera vista, " +
                                 "Harry no es un chico común y corriente: ¡es un verdadero mago"},
             {"Titulo: Frankenstein", "Autor: Marry Shelly", "Editorial: Lackington",
                     "SIPNOSIS \n Frankenstein es una historia macabra en la que Víctor, un joven ávido de conocimientos científicos, " +
-                        "se obsesiona por lograr el mayor reto posible en el mundo científico: dar vida a un cuerpo muerto. " +
+                        "se obsesiona por lograr el mayor reto posible en el mundo científico: dar vida a un cuerpo muerto." +
                          "Su éxito será su condena, la creación de un monstruo estremecedor que, en respuesta a su rechazo " +
                          "por todos se entrega por completo a saciar una sed de venganza hacia su creador, culpable de su desgracia, " +
-                         "y hacia todo lo que éste ama, tornando en muerte todo alrededor de Víctor. El monstruo, enfermo de soledad," +
+                         "y hacia todo lo que éste ama, tornando en muerte todo alrededor de Víctor. El monstruo, enfermo de soledad, " +
                          "solicita una compañera a su creador a cambio de desaparecer para siempre, pero Víctor se niega a ello, " +
                          "provocando así que la única salida hacia la paz y el descanso sea el fin de uno de los dos."},
             {"Titulo: Cumanda", "Autor: Juan Leon Mera", "Editorial: Desconocido",
                     "SIPNOSIS \n Tras perder a su familia en un incendio, Carlos y su padre dejan su "+
-                        "hacienda en la Sierra y se trasladan a la inexplorada Amazonía. "+
-                        "Allí, el joven se enamora de Cumandá, una bella indígena. "+
+                        "hacienda en la Sierra y se trasladan a la inexplorada Amazonía."+
+                        "Allí, el joven se enamora de Cumandá, una bella indígena."+
                         "Sin embargo, las disputas entre aborígenes y colonos y un terrible "+
-                        "pasado transformarán esta historia de amor en una tragedia. "+
+                        "pasado transformarán esta historia de amor en una tragedia."+
                         "Un clásico de la literatura ecuatoriana."},
             {"Titulo: Cien años de Soledad", "Autor: Gabriel García Márquez",
                     "Editorial: Desconocido",
                     "SIPNOSIS \n Entre la boda de José Arcadio Buendía con Amelia Iguarán hasta "+
-                        "la maldición de Aureliano Babilonia transcurre todo un siglo. "+
+                        "la maldición de Aureliano Babilonia transcurre todo un siglo."+
                         "Cien años de soledad para una estirpe única, fantástica, capaz "+
                         "de fundar una ciudad tan especial como Macondo y de engendrar "+
                         "niños con cola de cerdo. En medio, una larga docena de personajes "+
@@ -63,10 +63,10 @@ public class MainActivity3 extends AppCompatActivity implements SearchView.OnQue
                             "a caperucita y comérsela también. Afortunadamente, un leñador" +
                             " que andaba por allí descubre al lobo durmiendo tras su comida," +
                             " y rescata a caperucita y su abuelita de la tripa del lobo, " +
-                            "sustituyéndolas por piedras que hacen que el lobo se ahoge al ir " +
+                            "sustituyéndolas por piedras que hacen que el lobo se ahogue al ir " +
                             "a beber al río."},
             {"Titulo: Patito Feo", "Autor: Hans Christian Andersen", "Editorial: Everest",
-                    "  SIPNOSIS \n Esta es la historia de un cisne que nace en un corral de patos. " +
+                    "  SIPNOSIS \n Esta es la historia de un cisne que nace en un corral de patos." +
                             "Ridiculizado por ser diferente, huye, pero donde quiera que va, " +
                             "tropieza con la misma incomprensión, hasta que al fin crece y se " +
                             "convierte en la más hermosa y admirada criatura."},
@@ -74,7 +74,7 @@ public class MainActivity3 extends AppCompatActivity implements SearchView.OnQue
                     "  SIPNOSIS \n Los tres cerditos viven con su madre hasta que un día ella les dice " +
                             "que tienen que independizarse. Se despiden de ella y cada uno construye " +
                             "una casa: de paja, de madera y de ladrillos. Pero el lobo feroz destruye " +
-                            "la casa de paja y la de madera y los cerditos huyen a casa de su hermano. " +
+                            "la casa de paja y la de madera y los cerditos huyen a casa de su hermano." +
                             "¿Conseguirán superar su miedo y vencer al lobo?"},
             {"Titulo: Hansel y Gretel", "Autor: Jacob Grimm y Wilhelm Grimm", "Editorial: Hermanos Grimm",
                     "  SIPNOSIS \n Hansel y Gretel es otra de las maravillosas historias que recopilaron " +
@@ -86,45 +86,44 @@ public class MainActivity3 extends AppCompatActivity implements SearchView.OnQue
                             "habita una bruja a quien también le gustan las golosinas... pero para ella " +
                             "las más suculentas son las que están recubiertas de niño tierno."},
             {"Título: Drácula", "Autor: Bram Stoker", "Editorial: Valdemar",
-            "SIPNOSOS \n Cuando Jonathan Harker viajó a Transilvania por asuntos de negocios, nunca"+
-                    "imaginó el peligro que corría. Su cliente, el magnético conde Drácula, lo mantiene"+
-                    "prisionero y Harker adivina sus oscuros propósitos: viajar a Londres para"+
+            "SIPNOSOS \n Cuando Jonathan Harker viajó a Transilvania por asuntos de negocios, nunca "+
+                    "imaginó el peligro que corría. Su cliente, el magnético conde Drácula, lo mantiene "+
+                    "prisionero y Harker adivina sus oscuros propósitos: viajar a Londres para "+
                     "sembrar el terror y cosechar víctimas con las que saciar su apetito de sangre."+
-                    "En Inglaterra, Lucy Westenra pasa de la felicidad por su futuro matrimonio a un"+
-                    "letargo inexplicable. Cada vez más débil, preocupa a sus amigos con su palidez y"+
-                    "unas extrañas heridas en el cuello. Su amigo, el doctor Seward, decide llamar al"+
-                    "profesor Van Helsing para que tome cartas en el asunto. Pronto, el círculo que"+
+                    "En Inglaterra, Lucy Westenra pasa de la felicidad por su futuro matrimonio a un "+
+                    "letargo inexplicable. Cada vez más débil, preocupa a sus amigos con su palidez y "+
+                    "unas extrañas heridas en el cuello. Su amigo, el doctor Seward, decide llamar al "+
+                    "profesor Van Helsing para que tome cartas en el asunto. Pronto, el círculo que "+
                     "rodea a Lucy va a vivir una experiencia aterradora que pondrá a prueba su valor."},
             {"Título: El misterio de Salem's Lot", "Autor: Stephen King", "Editorial: Doubleday",
-            "SINOPSIS \n Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias,"+
-            "pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes...."+
-            "Veinte años atrás, por una apuesta infantil, Ben Mears entró en la casa de los Marsten. Y lo que"+
-            "vio entonces aún recorre sus pesadillas. Ahora, como escritor consagrado, vuelve a Salem's Lot"+
+            "SINOPSIS \n Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias, "+
+            "pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes..."+
+            "Veinte años atrás, por una apuesta infantil, Ben Mears entró en la casa de los Marsten. Y lo que "+
+            "vio entonces aún recorre sus pesadillas. Ahora, como escritor consagrado, vuelve a Salem's Lot "+
             "para exorcizar sus fantasmas. Salem's Lot es un pueblo tranquilo y adormilado donde nunca pasa nada..., "+
-            "excepto la antigua tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio. Y el"+
-            "tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio. Y el"+
-            "animales que mueren desangrados... Y la espantosa presencia de Ellos, quienesquiera que sean ELLOS."},
+            "excepto la antigua tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio."+
+            "Animales que mueren desangrados... Y la espantosa presencia de Ellos, quienesquiera que sean ELLOS."},
             {"Título: Soy leyenda", "Autor: Richard Matheson","Editorial: Minotauro",
-            "SINPSIS \n Robert Neville es el único superviviente de una guerra bacteriológica que ha asolado el planeta"+
-                    "y convertido al resto de la humanidad en vampiros. Su vida se ha reducido a asesinar el máximo número"+
-                    "posible de estos seres sanguinarios durante el día, y a soportar su asedio cada noche.  Para ellos, el auténtico"+
-                    "monstruo es este hombre que lucha por subsistir en un nuevo orden establecido. Todo un clásico en su género,"+
-                    "éste es un perturbador relato sobre la soledad y el aislamiento y una reflexión sobre los"+
-                    "binomios como normalidad y anormalidad, bien y mal, que se evidencian como una mera convención derivada"+
+            "SINPSIS \n Robert Neville es el único superviviente de una guerra bacteriológica que ha asolado el planeta "+
+                    "y convertido al resto de la humanidad en vampiros. Su vida se ha reducido a asesinar el máximo número "+
+                    "posible de estos seres sanguinarios durante el día, y a soportar su asedio cada noche. Para ellos, el auténtico "+
+                    "monstruo es este hombre que lucha por subsistir en un nuevo orden establecido. Todo un clásico en su género, "+
+                    "éste es un perturbador relato sobre la soledad y el aislamiento y una reflexión sobre los "+
+                    "binomios como normalidad y anormalidad, bien y mal, que se evidencian como una mera convención derivada "+
                     "del temor y el desconcierto ante lo diferente. Soy Leyenda está considerada la mejor novela de vampiros  "+
                     "del siglo XX y ha inspirado tres películas, la más reciente, protagonizada por Will Smith."}
             {"Título: El Hobbit", "Autor: J. R. R. Tolkien", "Editorial: Booket",
             "SINOPSIS \n Smaug parecía profundamente dormido cuando Bilbo espió una vez más desde la entrada."+
-            "¡Pero fingía estar dormido! ¡Estaba vigilando la entrada del túnel!... Sacado de su cómodo agujero-hobbit"+
-            "por Gandalf y una banda de enanos, Bilbo se encuentra de pronto en medio de una conspiración que pretende"+
+            "¡Pero fingía estar dormido! ¡Estaba vigilando la entrada del túnel!... Sacado de su cómodo agujero-hobbit "+
+            "por Gandalf y una banda de enanos, Bilbo se encuentra de pronto en medio de una conspiración que pretende "+
             "apoderarse del tesoro de Smaug el Magnífico, un enorme y muy peligroso dragón..."},
             {"Título: El conde de Montecristo", "Autor: Alejandro Dumas", "Editorial: Racuda Ediciones",
             "SINOPSIS \n El conde de Montecristo es uno de los clásicos más populares de todos los tiempos. Desde su publicación, "+
             "en 1844, no ha dejado de seducir al gran público con la inolvidable historia de su protagonista. Edmond Dantés es "+
-            "un joven marinero, honrado y cándido, que lleva una existencia tranquila. Quiere casarse con la hermosa Mercedes,"+
+            "un joven marinero, honrado y cándido, que lleva una existencia tranquila. Quiere casarse con la hermosa Mercedes, "+
             "pero su vida se verá arruinada cuando su mejor amigo, Ferdinand, deseoso de conquistar a su prometida, le traicione "+
             "vilmente. Condenado a cumplir una condena que no merece en la siniestra prisión del castillo de If, Edmond "+
-            " vivirá una larga pesadilla de trece años. Obsesionado por su inesperado destino, ejará de lado sus convicciones en torno"+
+            "vivirá una larga pesadilla de trece años. Obsesionado por su inesperado destino, dejará de lado sus convicciones en torno "+
             "al bien y al mal, y se dedicará a tramar la venganza perfecta."},
         {"Título: Guia del Autoestopista Galactico", "Autor: Douglas Adams", "Editorial: Anagrama",
             "SINOPSIS \n , Un jueves a la hora de comer, la Tierra es demolida para poder construir una nueva autopista hiperespacial."+

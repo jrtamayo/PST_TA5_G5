709f68896697046098f43d0090f91fa823dfcf9a
Braulio Paredes <84678447+beparede@users.noreply.github.com>
2021-08-06 10:41:34 -0500

    Correcciones ortográficas y de formato

--- app/src/main/java/com/example/amst5/VentanPorCategoria.java
+++ app/src/main/java/com/example/amst5/VentanPorCategoria.java
@@ -14,39 +14,42 @@ public class VentanPorCategoria extends AppCompatActivity {
     ListView listaTerror;
     String[][] datoster = {
             {"Titulo: Frankenstein", "Autor: Marry Shelly", "Editorial: Lackington",
-                    "SINOPSIS:\n Frankenstein es una historia macabra en la que Víctor, un joven ávido de conocimientos científicos,"+
-            " se obsesiona por lograr el mayor reto posible en el mundo científico: dar vida a un cuerpo muerto."+
-            "Su éxito será su condena, la creación de un monstruo estremecedor que,"+ "n respuesta a su rechazo por todos se entrega por completo a"
-            +"saciar una sed de venganza hacia su creador, culpable de su desgracia,"+ "y hacia todo lo que éste ama, tornando en muerte todo"+
-            "alrededor de Víctor. El monstruo,"+"enfermo de soledad, solicita una compañera a su creador a cambio de desaparecer para siempre,"+
-            "pero Víctor se niega a ello, provocando así que la única salida hacia la"+"paz y el descanso sea el fin de uno de los dos."},
+                    "SINOPSIS:\n Frankenstein es una historia macabra en la que Víctor, un joven ávido de conocimientos científicos, "+
+                            "se obsesiona por lograr el mayor reto posible en el mundo científico: dar vida a un cuerpo muerto."+
+                            "Su éxito será su condena, la creación de un monstruo estremecedor que, "+ 
+                            "n respuesta a su rechazo por todos se entrega por completo a "+
+                            "saciar una sed de venganza hacia su creador, culpable de su desgracia, "+
+                            "y hacia todo lo que éste ama, tornando en muerte todo "+
+                            "alrededor de Víctor. El monstruo, "+
+                            "enfermo de soledad, solicita una compañera a su creador a cambio de desaparecer para siempre, "+
+                            "pero Víctor se niega a ello, provocando así que la única salida hacia la"+
+                            "paz y el descanso sea el fin de uno de los dos."},
             {"Título: Drácula", "Autor: Bram Stoker", "Editorial: Valdemar",
-                    "SIPNOSOS \n Cuando Jonathan Harker viajó a Transilvania por asuntos de negocios, nunca"+
-                            "imaginó el peligro que corría. Su cliente, el magnético conde Drácula, lo mantiene"+
-                            "prisionero y Harker adivina sus oscuros propósitos: viajar a Londres para"+
+                    "SIPNOSOS \n Cuando Jonathan Harker viajó a Transilvania por asuntos de negocios, nunca "+
+                            "imaginó el peligro que corría. Su cliente, el magnético conde Drácula, lo mantiene "+
+                            "prisionero y Harker adivina sus oscuros propósitos: viajar a Londres para "+
                             "sembrar el terror y cosechar víctimas con las que saciar su apetito de sangre."+
-                            "En Inglaterra, Lucy Westenra pasa de la felicidad por su futuro matrimonio a un"+
-                            "letargo inexplicable. Cada vez más débil, preocupa a sus amigos con su palidez y"+
-                            "unas extrañas heridas en el cuello. Su amigo, el doctor Seward, decide llamar al"+
-                            "profesor Van Helsing para que tome cartas en el asunto. Pronto, el círculo que"+
+                            "En Inglaterra, Lucy Westenra pasa de la felicidad por su futuro matrimonio a un "+
+                            "letargo inexplicable. Cada vez más débil, preocupa a sus amigos con su palidez y "+
+                            "unas extrañas heridas en el cuello. Su amigo, el doctor Seward, decide llamar al "+
+                            "profesor Van Helsing para que tome cartas en el asunto. Pronto, el círculo que "+
                             "rodea a Lucy va a vivir una experiencia aterradora que pondrá a prueba su valor."},
             {"Título: El misterio de Salem's Lot", "Autor: Stephen King", "Editorial: Doubleday",
-                    "SINOPSIS \n Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias,"+
-                            "pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes...."+
-                            "Veinte años atrás, por una apuesta infantil, Ben Mears entró en la casa de los Marsten. Y lo que"+
-                            "vio entonces aún recorre sus pesadillas. Ahora, como escritor consagrado, vuelve a Salem's Lot"+
+                    "SINOPSIS \n Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias, "+
+                            "pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes..."+
+                            "Veinte años atrás, por una apuesta infantil, Ben Mears entró en la casa de los Marsten. Y lo que "+
+                            "vio entonces aún recorre sus pesadillas. Ahora, como escritor consagrado, vuelve a Salem's Lot "+
                             "para exorcizar sus fantasmas. Salem's Lot es un pueblo tranquilo y adormilado donde nunca pasa nada..., "+
-                            "excepto la antigua tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio. Y el"+
-                            "tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio. Y el"+
-                            "animales que mueren desangrados... Y la espantosa presencia de Ellos, quienesquiera que sean ELLOS."},
+                            "excepto la antigua tragedia de la casa de los Marsten. Y el perro muerto colgado de la verja del cementerio."+
+                            "Animales que mueren desangrados... Y la espantosa presencia de Ellos, quienesquiera que sean ELLOS."},
             {"Título: Soy leyenda", "Autor: Richard Matheson","Editorial: Minotauro",
-                    "SINPSIS \n Robert Neville es el único superviviente de una guerra bacteriológica que ha asolado el planeta"+
-                            "y convertido al resto de la humanidad en vampiros. Su vida se ha reducido a asesinar el máximo número"+
-                            "posible de estos seres sanguinarios durante el día, y a soportar su asedio cada noche.  Para ellos, el auténtico"+
-                            "monstruo es este hombre que lucha por subsistir en un nuevo orden establecido. Todo un clásico en su género,"+
-                            "éste es un perturbador relato sobre la soledad y el aislamiento y una reflexión sobre los"+
-                            "binomios como normalidad y anormalidad, bien y mal, que se evidencian como una mera convención derivada"+
-                            "del temor y el desconcierto ante lo diferente. Soy Leyenda está considerada la mejor novela de vampiros  "+
+                    "SINPSIS \n Robert Neville es el único superviviente de una guerra bacteriológica que ha asolado el planeta "+
+                            "y convertido al resto de la humanidad en vampiros. Su vida se ha reducido a asesinar el máximo número "+
+                            "posible de estos seres sanguinarios durante el día, y a soportar su asedio cada noche. Para ellos, el auténtico "+
+                            "monstruo es este hombre que lucha por subsistir en un nuevo orden establecido. Todo un clásico en su género, "+
+                            "éste es un perturbador relato sobre la soledad y el aislamiento y una reflexión sobre los "+
+                            "binomios como normalidad y anormalidad, bien y mal, que se evidencian como una mera convención derivada "+
+                            "del temor y el desconcierto ante lo diferente. Soy Leyenda está considerada la mejor novela de vampiros "+
                             "del siglo XX y ha inspirado tres películas, la más reciente, protagonizada por Will Smith."}
     };
 
